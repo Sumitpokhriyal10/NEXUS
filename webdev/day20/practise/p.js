7997022eac5f7635be7ea2e9b074b3043ba49f33
Sumit Pokhriyal <sumitpokhriyalonezero@gmail.com>
2025-06-07 08:07:36 -0700

    accessviaselectors

---
+++ webdev/day20/practise/p.js
@@ -0,0 +1,34 @@
+//const id =document.querySelector('#first');
+//id.innerHTML="sumit";
+
+//const id2=document.querySelector('.header2');
+//console.log(id2);
+
+//how to iterate over node list
+
+//1 for each
+//obj.forEach((val)=>{  console.log(val) });
+
+//2 for of
+//for(let val of obj){ console.log(val)  }
+
+//  3for
+//for(i=0;i<obj.length;i++){  console.log(obj[i])}
+
+
+//covert nodelist to array
+//Array.from(obj)
+
+
+//const obj =document.querySelector('li');
+//console.log(obj.parentElement);
+//console.log(obj.parentNode);
+
+
+const obj =document.querySelector('li');
+//console.log(obj.firstChild);
+//console.log(obj.firstElementChild);
+//console.log(obj.lastChild);
+//console.log(obj.nextElementSibling);
+
+obj.textContent
