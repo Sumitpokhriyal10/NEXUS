d7160c7f97f2607e7cb3a2733fe0b8985f08995a
Sumit Pokhriyal <sumitpokhriyalonezero@gmail.com>
2025-06-06 22:25:37 -0700

    day20

--- webdev/day20/summi.js
+++ webdev/day20/summi.js
@@ -1,1 +1,20 @@
-console.log("sumit");
+
+function timering(){
+const timer= document.getElementById("root");
+const date=new Date();
+const indiantime=date.toLocaleTimeString();
+timer.innerHTML=indiantime;
+
+}
+
+
+setInterval(timering,1000);
+
+const timer= document.getElementById("root");
+timer.style.display = "flex";
+timer.style.justifyContent = "center"; // optional
+timer.style.alignItems = "center"
+timer.style.backgroundColor="orange";
+timer.style.fontSize="100px";
+timer.style.height="100vh";
+
