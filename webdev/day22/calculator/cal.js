331695ca78ff0fc3e956b92880094dbbde7dd351
Sumit Pokhriyal <sumitpokhriyalonezero@gmail.com>
2025-06-09 00:19:14 -0700

    removed bugfix

--- webdev/day22/calculator/cal.js
+++ webdev/day22/calculator/cal.js
@@ -6,7 +6,7 @@ button.addEventListener("click", () => {
   const num1 = document.getElementById("first");
   const add1 = Number(num1.value);
   const num2 = document.getElementById("second");
-  const add2 = Number(num1.value);
+  const add2 = Number(num2.value);
 
   const result = add1 + add2;
 
