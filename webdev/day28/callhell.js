44143c6097ad93bae58f306fa4719fe77a249c4a
Sumit Pokhriyal <sumitpokhriyalonezero@gmail.com>
2025-06-15 23:13:24 -0700

    day28 callback

---
+++ webdev/day28/callhell.js
@@ -0,0 +1,30 @@
+//call back hell
+
+function fetchuser(callback) {
+  console.log("fetching the user detail.....");
+  setTimeout(() => {
+    console.log("data fetched succesfully");
+
+    const obj = {
+      name: "sumit",
+      age: 23,
+      city: "uttrakhand",
+    };
+    callback(obj);
+  }, 2000);
+}
+
+function greet(obj) {
+  console.log(`hello ${obj.name}`);
+}
+
+function meet(obj) {
+  console.log(` hi  ${obj.name}  i will meet you in delhi `);
+}
+
+function age(obj) {
+  console.log(`the age is ${obj.age}`);
+}
+
+fetchuser(greet);
+//fetchuser(age);
