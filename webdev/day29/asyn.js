71ad70c0d59a2becc225a4b8cd376856e2c2f004
Sumit Pokhriyal <sumitpokhriyalonezero@gmail.com>
2025-06-16 22:15:16 -0700

    async task and web api

---
+++ webdev/day29/asyn.js
@@ -0,0 +1,31 @@
+//console.log("hello coder");
+
+//function meet() {
+  //const arr = [2, 4, 6];
+  //console.log(arr[0]);
+//}
+
+//function greet() {
+  //const a = 2 + 3;
+  //console.log(a);
+  //meet();
+  //console.log(a * a);
+//}
+//greet();
+//console.log("program ended");
+  
+//async task
+
+//console.log("hello coder army");
+
+//setTimeout(()=>{
+   // const a = 3+4;
+  //  console.log(a);
+//},5000);
+
+//let b=20;
+//let arr =[20,30,11];
+
+//for(let i of arr)
+ //   console.log(i*b);
+
